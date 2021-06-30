9c90c74a5a75c5bd71c43f46ad1b255efc234a37
Mukhtiar <muhammad.mukhtiar@norgic.com>
2021-06-30 12:19:02 +0500

    Bug Fixing

--- src/app/components/chat/add-group/add-group.component.ts
+++ src/app/components/chat/add-group/add-group.component.ts
@@ -93,9 +93,18 @@ export class AddGroupComponent implements OnInit {
             this.AllUsers = [];
           } else {
             this.AllUsers = res.users;
+            this.setAlreadySelected();
           }
         })
     }
+  }
+
+  setAlreadySelected() {
+    this.selectedUsers.map(user => {
+      const userIndex = this.AllUsers.find(alluser => alluser.user_id == user.user_id);
+      if (userIndex) userIndex['selected'] = true;
+    });
+    this.changeDetector.detectChanges();
   }
 
   selectContact(contact) {
