d6a3e00a5cdc6021e05b6ca8954de18ece554791
RameezNorgic <rameez.ahmad@norgic.com>
2022-08-25 18:22:11 +0500

    updated the endpoint for vdotok sdk and updated the project-id

--- src/app/shared/services/pubsub.service.ts
+++ src/app/shared/services/pubsub.service.ts
@@ -10,8 +10,9 @@ export class PubsubService {
   public initConfigure(): void {
     const user = StorageService.getUserData();
     this.Client = new CVDOTOK.ManyToMany({
-      projectID: "143LV8M8",
-      secret: "3d9686b635b15b5bc2d19800407609fa",
+      // projectID: "143LV8M8",
+      projectID: "6NE92I",
+      // secret: "3d9686b635b15b5bc2d19800407609fa",
       host: `${user.media_server_map.protocol}://${user.media_server_map.host}:${user.media_server_map.port}/${user.media_server_map.end_point}`
     });
     this.Client.on("connected", (res) => {
